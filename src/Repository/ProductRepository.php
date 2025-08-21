29efa3c6e6a697883f2dfb490f8fab6a5d12d486
Artem Holovanov <artem.holovanov@stfalcon.com>
2025-08-21 17:47:07 +0300

    Refactoring

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -5,6 +5,10 @@ namespace CommonBundle\Repository;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
+/**
+ * @template T of object
+ * @extends ServiceEntityRepository<T>
+ */
 abstract class ProductRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry, string $entityClass)
