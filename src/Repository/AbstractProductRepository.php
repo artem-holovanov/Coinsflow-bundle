77509891e2f4412168610d77e3429af409246dfe
Artem Holovanov <artem.holovanov@stfalcon.com>
2025-08-21 17:54:58 +0300

    Refactoring

--- src/Repository/AbstractProductRepository.php
+++ src/Repository/AbstractProductRepository.php
@@ -9,8 +9,12 @@ use Doctrine\Persistence\ManagerRegistry;
  * @template T of object
  * @extends ServiceEntityRepository<T>
  */
-abstract class ProductRepository extends ServiceEntityRepository
+abstract class AbstractProductRepository extends ServiceEntityRepository
 {
+    /**
+     * @param ManagerRegistry $registry
+     * @param class-string<T> $entityClass
+     */
     public function __construct(ManagerRegistry $registry, string $entityClass)
     {
         parent::__construct($registry, $entityClass);
